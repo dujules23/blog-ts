f6e021d383eddee92d5813d67dfaf8cf409b9183
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-06 18:07:50 -0400

    added readPostsInfo function, console log shows files being read as expected

--- pages/api/posts.ts
+++ pages/api/posts.ts
@@ -1,15 +1,29 @@
 import { NextApiHandler } from "next"
+import fs from 'fs'
+import path from 'path'
 
 const handler: NextApiHandler = (req, res) => {
   const { method } = req;
 
   switch (method) {
-    case "GET": 
-      return res.json({ok: true})
+    case "GET": {
+      const data = readPostsInfo()
+      return res.json({ postInfo: data })
+    }
+      
     default: 
       return res.status(404).send("Not Found")
   }
+}
 
+const readPostsInfo = () => {
+  const dirPathToRead = path.join(process.cwd(), 'pages/posts')
+  const dirs = fs.readdirSync(dirPathToRead)
+  dirs.map(filename => {
+    const filePathToRead = path.join(process.cwd(), 'pages/posts/' + filename)
+    const fileContent = fs.readFileSync(filePathToRead, { encoding: 'utf-8'})
+    console.log(fileContent)
+  })
 }
 
 export default handler
