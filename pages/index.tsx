b939dabcc8cf56feafe96ee8412159a21e5df267
Durrell Jules <71855759+dujules23@users.noreply.github.com>
2023-07-10 15:18:27 -0400

    changed the homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,33 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import BlogCard from '@/components/BlogCard';
+import { readPostsInfo } from '@/lib/helper';
+import { PostApiResponse } from '@/utils/types';
+import { GetStaticProps, InferGetStaticPropsType, NextPage } from 'next';
+import { useEffect, useState } from 'react';
+ 
 
-const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
-  return (
-   <h1 className='text-3xl font-bold underline text-gray-400∆'>Hello World!</h1> ) }
+
+export const getStaticProps = async () => {
+  
+  // const { postInfo }: PostApiResponse = await fetch('http://localhost:3000/api/posts').then(data => data.json())
+   
+  const postInfo: PostApiResponse = readPostsInfo()
+
+  return {
+    props:{posts: postInfo}
+  }
+}
+
+type Props = InferGetStaticPropsType<typeof getStaticProps>
+
+const Blogs: NextPage<Props> = ({posts}) => {
+   
+
+   return (
+   <div className=' max-w-3xl mx-auto p-5 space-y-5'>
+    {posts.map(post => <BlogCard key={post.slug} title={post.title} description={post.meta} slug={post.slug}/>)}
+   </div>
+   )
+}
+
+export default Blogs
